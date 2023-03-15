74c8db65b749ae2c284f765be35d7c73916fb989
NammiDivyaDeepika <121595794+NammiDivyaDeepika@users.noreply.github.com>
2023-03-15 20:36:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from PIL import Image
 
 st. set_page_config(layout="wide") 
 image = Image.open("Saje.jpg")
-
 st.header('Dear International Students in Cologne. Are you following the Saje Mystery? We’ve glad you’ve found us.')
 st.subheader('Here’s the next part of the story:')
 st.markdown("[Voice message left at 22:16]")
