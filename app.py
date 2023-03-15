319e02c23ec36231b4006469ebdbfe8ce6a207bc
NammiDivyaDeepika <121595794+NammiDivyaDeepika@users.noreply.github.com>
2023-03-15 20:22:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ import streamlit as st
 from PIL import Image
 
 st. set_page_config(layout="wide") 
-#image = Image.open("C:\\Users\\divya\\OneDrive\\Desktop\\me\\AKKA\\Us.JPG")
-image = Image.open("/home/meet/app/Us.JPG")
+image = Image.open("Us.JPG")
+st.header('Dear International Students in Cologne. Are you following the Saje Mystery? We’ve glad you’ve found us.')
+st.subheader('Here’s the next part of the story:')
+st.markdown("[Voice message left at 22:16]")
+st.markdown("Hey Nadia, this is Jake. Just checking in after my Saje interview. Went pretty smoothly, spoke to a guy at the Fresenius “Pioneer Lab” it’s called. It’s like a start-hub at the university that supports students or founders to develop their ideas. Anyway, they’ve been supporting Sanvi Sharma and Jeremy Okana-Cole from, like, the beginning, giving them lots of advice and guidance and the like, and also put them in touch with a big-name mentor from Silicon Valley whose been working closely with them for a while. Her name is…Lynn Barker..Lynn..wait a moment, no that’s right, Lynn Barker. Yeah, so she’s apparently a big deal over in LA working as a consultant with all the big-name companies but also likes to give back and support start-ups on this side of the pond for free. You know, like pro-bono work. Been listening around, some people are saying that she’s super good and it’s mainly because of her that the Saje thing has taken off.
 
-st.image(image, caption='Just The Four Of Us')
-st.markdown("My heart blooms with joy having you as my sister in my life. One sister to another, one woman to another- you are the best! I know that at the end of the day, my sister will always look up to me and make sure my comfort and journey are smooth. I love you so much. Thank you for being with me. They say having a best friend is the best thing one can ask for. I say having a best friend in a sister is the most blessing thing for one. You are the best, my sister. Take care. You may think I am happy to share my clothes and makeup with you. The truth is, I love sharing my childhood with you the most. Thank you for being my sister. I sometimes wonder how God knew that I needed a sister exactly like you. Strong, passionate, and kind- all in one body. Take my unlimited love and respect, sissy. I am put in this world because you are here for me, standing strong to support your sister against and for everything. Thank you, love.")
+st.markdown("And I know what you’re going to ask, miss editor, am I going to contact Lynn Barker and get an interview as well? I’m one step ahead of you, managed to get her on the phone straight away. She was in a meeting so didn’t have much time to talk but seemed really nice. We’ve set up a call for tomorrow morning to go over her involvement with Saje and how she’s helped them out. I think that’ll add a nice touch to the story.")
+
+st.markdown("So, I’ll work on that and send you a first draft of the article by Thursday. Don’t worry, we’ll still get it in next week’s copy.")
+
+st.markdown("Alright, talk to you soon.")
+
+st.markdown("[End of message]")
+
 st. write('***')
+st.success("Curious to find out what happens next? You’ll find out more when we meet on campus on Thursday.")
+st.subheader("The Fresenians")
