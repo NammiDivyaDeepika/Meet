2a6a0d7dcd98a96833884dc476867a1e06b4e3e7
NammiDivyaDeepika <121595794+NammiDivyaDeepika@users.noreply.github.com>
2023-03-24 11:56:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,20 +16,12 @@ with col3:
 st.success("Congratulations…! You are just one step away from solving the mystery. Your persistence and analytical skills have led you to this point, and there is no doubt that you will be successful in completing this final task.")
 col4, col5, col6, col7, col8,= st.columns(3)
 
-with col4:
-    st.write(' ')
-with col5:
-    st.write(' ')
-with col6:  
-    if st.button('Scan Me'):
+
+if st.button('Scan Me'):
              qr_image = Image.open('QR.jpg')
              st.image(qr_image, width=200)
-    else:
+else:
              st.write('Click the button to scan the QR code')
-with col7:
-    st.write(' ')
-with col8:
-    st.write(' ')
     
     
 
