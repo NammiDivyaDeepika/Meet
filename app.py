4bc09cd668af53dab013a5cbf2335963b06bce93
NammiDivyaDeepika <121595794+NammiDivyaDeepika@users.noreply.github.com>
2023-03-24 05:49:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-
-st. set_page_config(layout="wide") 
+st.set_page_config(layout="wide")
 
 col1, col2, col3 = st.columns(3)
 
@@ -14,25 +13,19 @@ with col2:
 with col3:
     st.write(' ')
 
-st.title('Dear International Students in Cologne. Are you following the Saje Mystery? We’ve glad you’ve found us.')
+st.title('Dear International Students in Cologne. Are you following the Saje Mystery? We’re glad you’ve found us.')
 st.success("Congratulations…! You are just one step away from solving the mystery. Your persistence and analytical skills have led you to this point, and there is no doubt that you will be successful in completing this final task.")
 st.caption('Here’s the next part of the story:')
 st.markdown("[Voice message left at 22:16]")
 st.markdown("Hey Nadia, this is Jake. Just checking in after my Saje interview. Went pretty smoothly, spoke to a guy at the Fresenius “Pioneer Lab” it’s called. It’s like a start-hub at the university that supports students or founders to develop their ideas. Anyway, they’ve been supporting Sanvi Sharma and Jeremy Okana-Cole from, like, the beginning, giving them lots of advice and guidance and the like, and also put them in touch with a big-name mentor from Silicon Valley whose been working closely with them for a while. Her name is…Lynn Barker..Lynn..wait a moment, no that’s right, Lynn Barker. Yeah, so she’s apparently a big deal over in LA working as a consultant with all the big-name companies but also likes to give back and support start-ups on this side of the pond for free. You know, like pro-bono work. Been listening around, some people are saying that she’s super good and it’s mainly because of her that the Saje thing has taken off.")
-
 st.markdown("And I know what you’re going to ask, miss editor, am I going to contact Lynn Barker and get an interview as well? I’m one step ahead of you, managed to get her on the phone straight away. She was in a meeting so didn’t have much time to talk but seemed really nice. We’ve set up a call for tomorrow morning to go over her involvement with Saje and how she’s helped them out. I think that’ll add a nice touch to the story.")
-
 st.markdown("So, I’ll work on that and send you a first draft of the article by Thursday. Don’t worry, we’ll still get it in next week’s copy.")
-
 st.markdown("Alright, talk to you soon.")
-
 st.markdown("[End of message]")
-
-st. write('***')
+st.write('***')
 st.success("Curious to find out what happens next? You’ll find out more when we meet on campus on Thursday.")
 if st.button('Scan Me'):
-    st.write('Scan the QR code')
+    st.image('QR.jpg')
 else:
-    image2 = Image.open('QR.jpg')
-    st.image(image2)
+    st.write('Click the button to scan the QR code')
 st.subheader("The Fresenians")
