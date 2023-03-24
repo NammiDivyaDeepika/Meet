4d20db33caf4772ee0377ca0b79642b4d3e9b6f4
NammiDivyaDeepika <121595794+NammiDivyaDeepika@users.noreply.github.com>
2023-03-24 05:33:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ with col3:
     st.write(' ')
 
 st.title('Dear International Students in Cologne. Are you following the Saje Mystery? We’ve glad you’ve found us.')
+st.success("Congratulations…! You are just one step away from solving the mystery. Your persistence and analytical skills have led you to this point, and there is no doubt that you will be successful in completing this final task.")
 st.caption('Here’s the next part of the story:')
 st.markdown("[Voice message left at 22:16]")
 st.markdown("Hey Nadia, this is Jake. Just checking in after my Saje interview. Went pretty smoothly, spoke to a guy at the Fresenius “Pioneer Lab” it’s called. It’s like a start-hub at the university that supports students or founders to develop their ideas. Anyway, they’ve been supporting Sanvi Sharma and Jeremy Okana-Cole from, like, the beginning, giving them lots of advice and guidance and the like, and also put them in touch with a big-name mentor from Silicon Valley whose been working closely with them for a while. Her name is…Lynn Barker..Lynn..wait a moment, no that’s right, Lynn Barker. Yeah, so she’s apparently a big deal over in LA working as a consultant with all the big-name companies but also likes to give back and support start-ups on this side of the pond for free. You know, like pro-bono work. Been listening around, some people are saying that she’s super good and it’s mainly because of her that the Saje thing has taken off.")
@@ -29,4 +30,9 @@ st.markdown("[End of message]")
 
 st. write('***')
 st.success("Curious to find out what happens next? You’ll find out more when we meet on campus on Thursday.")
+if st.button('Scan Me'):
+    st.write('Scan the QR code')
+else:
+    image2 = Image.open('QR.jpg')
+    st.image(image2)
 st.subheader("The Fresenians")
